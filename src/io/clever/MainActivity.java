c5edd6a501f913fee8aa8171f8d1c4f9f03186db
Russell Beattie <russ@russellbeattie.com>
2012-09-09 02:25:18 -0700

    Lots of tweaks...

--- src/io/clever/MainActivity.java
+++ src/io/clever/MainActivity.java
@@ -2,9 +2,12 @@ package io.clever;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
 import android.webkit.*;
 import android.webkit.WebSettings.*;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends Activity {
@@ -18,34 +21,60 @@ public class MainActivity extends Activity {
         
         setContentView(R.layout.activity_main);
         
-        webView = (WebView) findViewById(R.id.webView1);
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout1);
         
+        linearLayout.setBackgroundColor(Color.BLACK);
+        
+        
+        webView = (WebView) findViewById(R.id.webView1);
         
         WebSettings webSettings = webView.getSettings();
         
         webView.setWebChromeClient(new WebChromeClient()); 
-        webView.setWebViewClient(new WebViewClient());
         
+        webView.setWebViewClient(new WebViewClient() {
+
+        	   public void onPageFinished(WebView view, String url) {
+        		   
+        		   Log.d("scale", view.getScale() + "");
+        	    
+        	   }
+        	   
+        	   public void onScaleChanged (WebView view, float oldScale, float newScale){
+        		   
+        		   Log.d("scale changed", oldScale + " - " + newScale);
+        		   
+        	   }
+        
+        });
+        
+        webView.setInitialScale(100);
+        webView.setMapTrackballToArrowKeys(true);
+        webView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        
+        webSettings.setDefaultZoom(ZoomDensity.FAR);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+        
+        webSettings.setSupportZoom(true);        
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
-        //webSettings.setBuiltInZoomControls(false);
-        //webSettings.setSupportZoom(false);
-        //webSettings.setTextZoom(80);   
-        //webSettings.setDefaultZoom(ZoomDensity.FAR);
-        webSettings.setSaveFormData(false);
         webSettings.setDatabaseEnabled(true); 
         webSettings.setDatabasePath("/data/data/" + webView.getContext().getPackageName() + "/databases/");
-        
+        webSettings.setSaveFormData(false);      
         webSettings.setLightTouchEnabled(false);
         webSettings.setLayoutAlgorithm(LayoutAlgorithm.NORMAL);
         webSettings.setUserAgentString("Mozilla/5.0 (X11; Linux i686) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.77 Large Screen Safari/534.24 GoogleTV");
 
-        webView.setInitialScale(90);
-        
-        webView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
         
         webView.loadUrl("file:///android_asset/www/index.html");
     
+        
+        
+        
     }
 
     @Override
